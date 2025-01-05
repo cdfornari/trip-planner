7ac734b39b0d8b5099f759e7b083457f6ef4dd87
Carlos Fornari <lilfornah@gmail.com>
2025-01-05 14:40:42 -0400

    fix: :bug:

--- libs/trip-plans/domain/value-objects/vehicle-plate.ts
+++ libs/trip-plans/domain/value-objects/vehicle-plate.ts
@@ -2,8 +2,7 @@ import { ValueObject } from 'libs/core/domain/value-object';
 
 export class VehiclePlate implements ValueObject<VehiclePlate> {
   constructor(private readonly _value: string) {
-    if (!/^([A-Z]{3}-\d{4})$/.test(_value))
-      throw new Error('INVALID_VEHICLE_PLATE');
+    if (this.value.length !== 7) throw new Error('INVALID_VEHICLE_PLATE');
   }
 
   get value(): string {
